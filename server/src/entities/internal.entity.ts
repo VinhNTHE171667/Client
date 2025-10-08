e050ec09c7dee7cfbfa8e8b8bac8f7278d581713
Loicute <thienhieuloi@gmail.com>
2025-10-08 19:30:47 +0700

    feat: add crud customer and crud category

--- server/src/entities/internal.entity.ts
+++ server/src/entities/internal.entity.ts
@@ -8,28 +8,34 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { RoleStaff } from './roleStaff.entity';
+import { Role } from './role.entity';
 
 @Entity()
-export class Staff {
+export class Internal {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ nullable: true })
   avatar?: string;
 
-  @Column()
+  @Column({ nullable: true })
   full_name: string;
 
-  @Column()
+  @Column({ nullable: true })
   phone: string;
 
   @Column()
   email: string;
 
-  @ManyToOne(() => RoleStaff, (role) => role.staffs, { eager: true })
+  @Column()
+  password: string;
+
+  @ManyToOne(() => Role, (role) => role.roles, { eager: true })
   @JoinColumn({ name: 'roleId' })
-  role: RoleStaff;
+  role: Role;
+
+  @Column({ nullable: true })
+  refreshToken?: string;
 
   @CreateDateColumn()
   createdAt: Date;
