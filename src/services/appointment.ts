2ff648050f865a1df348d567c35e8681b54ee799
Tan Kim <letankim2003@gmail.com>
2025-12-11 05:20:13 +0700

    update

--- src/services/appointment.ts
+++ src/services/appointment.ts
@@ -471,6 +471,27 @@ export const appointmentApi = createApi({
       }),
     }),
 
+    updateAppointmentStatusArrived: build.mutation<
+      AppointmentProps,
+      { appointmentId: string; staff: { id: string } }
+    >({
+      query: ({ appointmentId, staff: { id } }) => ({
+        url: `/appointment/${appointmentId}/arrived`,
+        method: "PATCH",
+        body: { id },
+      }),
+    }),
+
+    updateAppointmentStatusInService: build.mutation<
+      AppointmentProps,
+      { appointmentId: string }
+    >({
+      query: ({ appointmentId }) => ({
+        url: `/appointment/${appointmentId}/in-service`,
+        method: "PATCH",
+      }),
+    }),
+
     createLinkPayment: build.mutation<
       { checkoutUrl: string },
       {
@@ -579,6 +600,8 @@ export const {
   useUpdateAppointmentStatusApprovedMutation,
   useUpdateAppointmentStatusRejectedMutation,
   useUpdateAppointmentMutationCompleteMutation,
+  useUpdateAppointmentStatusArrivedMutation,
+  useUpdateAppointmentStatusInServiceMutation,
 
   useGetAppointmentsManagedByDoctorMutation,
   useGetAppointmentsBookedByDoctorMutation,
